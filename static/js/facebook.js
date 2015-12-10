65d19f10b74e85fac9889641822ccc39dfdb19be
Louise Olofsson <louise.olofsson@hotmail.se>
2015-12-10 18:58:28 +0100

    facebook

--- static/js/facebook.js
+++ static/js/facebook.js
@@ -29,27 +29,6 @@ function statusChangeCallback(response) {
     });
   }
 
-    window.fbAsyncInit = function() {
-    FB.init({
-    appId      : '101921540181193',
-    cookie      : true,    
-    xfbml      : true,
-    version    : 'v2.5'
-            });
-    };
-
-    (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s); 
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    
-
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
