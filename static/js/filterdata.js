7bd4307957e36a1982d42b1850ed0d3222a6fbc3
Ztarmaestro <erkkar2@gmail.com>
2015-12-11 12:15:50 +0100

    fix error

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -10,12 +10,17 @@ function filterEvent(data){
 	Info = document.getElementById('Info');
 	Preview = document.getElementById('Preview');
 
-
+	console.log(data.Event_name);
 	Event_name.innerHTML = Event_name.innerHTML + data.Event_name;
+	console.log(data.Address);
 	Address.innerHTML = Address.innerHTML + data.Address;
+	console.log(data.Zipcode);
 	Zipcode.innerHTML = Zipcode.innerHTML + data.Zipcode;
+	console.log(data.Date);
 	date.innerHTML = date.innerHTML + data.Date;
+	console.log(data.Info);
 	Info.innerHTML = Info.innerHTML + data.Info;
+	console.log(data.Preview);
 	Preview.innerHTML = Preview.innerHTML + data.Preview;
 
 }
