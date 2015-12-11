539c8aacb3ab36b08f6ccdd02626a390cb051ac6
Ztarmaestro <erkkar2@gmail.com>
2015-12-11 11:28:07 +0100

    fix of content

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -3,24 +3,24 @@ function filterEvent(data){
 
 	console.log(data);
 	
-	event_name = document.getElementById('Event_name');
-	address = document.getElementById('Address');
-	zipcode = document.getElementById('Zipcode');
-	date = document.getElementById('Date');
+	Event_name = document.getElementById('Event_name');
+	Address = document.getElementById('Address');
+	Zipcode = document.getElementById('Zipcode');
+	Date = document.getElementById('Date');
 	Info = document.getElementById('Info');
-	user = document.getElementById('User');
-	event_id = document.getElementById('Event_ID');
-	preview = document.getElementById('Preview');
+	User = document.getElementById('User');
+	Event_id = document.getElementById('Event_ID');
+	Preview = document.getElementById('Preview');
 	
 
-	event_name.innerHTML = data.Event_name;
-	address.innerHTML = data.address;
-	zipcode.innerHTML = data.zipcode;
-	date.innerHTML = data.date;
+	Event_name.innerHTML = data.Event_name;
+	Address.innerHTML = data.Address;
+	Zipcode.innerHTML = data.Zipcode;
+	Date.innerHTML = data.Date;
 	Info.innerHTML = data.Info;
-	user.innerHTML = data.user;
-	event_id.innerHTML = data.Event_ID;
-	preview.innerHTML = data.preview
+	User.innerHTML = data.User;
+	Event_id.innerHTML = data.Event_ID;
+	Preview.innerHTML = data.Preview;
 
 	console.log(data.Event_ID);
 	console.log(data.Event_name);
