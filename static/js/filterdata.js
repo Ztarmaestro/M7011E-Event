c303d99cf41b5b68558f27aebf279c870cec5ba0
Ztarmaestro <erkkar2@gmail.com>
2015-12-11 11:23:34 +0100

    added more content from db to site

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -2,12 +2,27 @@
 function filterEvent(data){
 
 	console.log(data);
-	printDiv = document.getElementById('Event_ID');
-	headline = document.getElementById('Event_Name');
-
-	printDiv.innerHTML = data.Event_ID;
-	headline.innerHTML = data.Event_Name;
 	
+	event_name = document.getElementById('Event_name');
+	address = document.getElementById('Address');
+	zipcode = document.getElementById('Zipcode');
+	date = document.getElementById('Date');
+	Info = document.getElementById('Info');
+	user = document.getElementById('User');
+	event_id = document.getElementById('Event_ID');
+	preview = document.getElementById('Preview');
+	
+
+	event_name.innerHTML data.Event_name;
+	address.innerHTML = data.address;
+	zipcode.innerHTML = data.zipcode;
+	date.innerHTML = data.date;
+	Info.innerHTML = data.Info;
+	user.innerHTML = data.user;
+	event_id.innerHTML = data.Event_ID;
+	preview.innerHTML = data.preview
+
 	console.log(data.Event_ID);
-	console.log(data.Event_Name);
+	console.log(data.Event_name);
+
 }
