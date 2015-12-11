365bb1489d4212ba70690aaa17657a61ff5689b4
Ztarmaestro <erkkar2@gmail.com>
2015-12-11 10:34:16 +0100

    added eventid to show

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -2,7 +2,7 @@
 function filterEvent(data){
 
 	console.log(data);
-	printDiv = document.getElementById('eventA');
+	printDiv = document.getElementById('Event_ID');
 	headline = document.getElementById('Event_Name');
 	printDiv.innerHTML = data;
 	headline.innerHTML = data.Event_Name;
