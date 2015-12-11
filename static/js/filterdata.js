db1fd24ba839ab06ffac2fbe912d53917c2e2f20
Ztarmaestro <erkkar2@gmail.com>
2015-12-11 11:04:31 +0100

    added some more logs

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -4,10 +4,10 @@ function filterEvent(data){
 	console.log(data);
 	printDiv = document.getElementById('Event_ID');
 	headline = document.getElementById('Event_Name');
-	console.log(Event_ID);
-	console.log(Event_Name);
-	printDiv.innerHTML = data;
+
+	printDiv.innerHTML = data.Event_ID;
 	headline.innerHTML = data.Event_Name;
-	console.log(data);
+	
+	console.log(data.Event_ID);
 	console.log(data.Event_Name);
 }
