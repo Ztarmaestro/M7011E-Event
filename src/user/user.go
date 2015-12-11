0fc3fc80f498ad4f6ff324825ce2b390251a27a8
Louise Olofsson <louise.olofsson@hotmail.se>
2015-12-11 16:33:01 +0100

    fixes in user.go

--- src/user/user.go
+++ src/user/user.go
@@ -16,45 +16,9 @@ import (
 )
 
 type User_tabel struct {
-	UserID    uint64 `json:"userID"`
-	IdToken   string `json:"id"`
+	UserID    uint64 `json:"UserID"`
+	IdToken   string `json:"IdToken"`
 	Username  string `json:"Username"`
-}
-
-/*
-	List all users in the db
-	!READY FOR TESTING!
-*/
-func ListAllUsers(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError) {
-	con, err := sql.Open("mymysql", "tcp:130.240.170.56:3306*mydb/dbadmin/eventdb")
-	if err != nil {
-		return nil, &HandlerError{err, "Local error opening DB", http.StatusInternalServerError}
-		log.Fatal(err)
-	}
-	defer con.Close()
-
-	rows, err := con.Query("select Username, userID from Users")
-	if err != nil {
-		return nil, &HandlerError{err, "Error in DB", http.StatusInternalServerError}
-		//log.Printf("No user with that ID")
-	}
-
-	var result []User // create an array of stairs
-	var userID uint64
-	var Username string
-
-	for rows.Next() {
-		user := new(User)
-		err = rows.Scan(&name, &uid)
-		if err != nil {
-			return result, &HandlerError{err, "Error in DB", http.StatusInternalServerError}
-		}
-		user.Username = name
-		user.userID = uid
-		result = append(result, *user)
-	}
-
-	return result, nil
 }
 
 /*
@@ -65,7 +29,7 @@ func GetUser(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError
 	//mux.Vars(r)["id"] grabs variables from the path
 	param := mux.Vars(r)["id"]
 	fmt.Println(param)
-	con, err := sql.Open("mymysql", "tcp:localhost:3306*M7011E/root/jaam")
+	con, err := sql.Open("mymysql", "tcp:130.240.170.56:3306*mydb/dbadmin/eventdb")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -82,28 +46,20 @@ func GetUser(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError
 
 	user := new(User)
 	for row.Next() {
-		var idToken string
-		var uid uint64
-		var name, lastname, photo string
+		var IdToken string
+		var UserID uint64
+		var Username string
 
 		fmt.Println(row)
 
-		if err := row.Scan(&uid, &name, &lastname, &idToken, &photo); err != nil {
+		if err := row.Scan(&IdToken, &UserID, &Username); err != nil {
 			log.Fatal(err)
 		}
-		user.IdToken = idToken
-		user.UserID = uid
-		user.FirstName = name
-		user.LastName = lastname
-		user.Photo = photo
+		//user.IdToken = id
+		//user.UserID = userid
+		//user.Username = name
+		
 	}
-	//fmt.Println(row)
-	//	user.UserID = row[0]
-	//	user.FirstName = row[1]
-	//	user.LastName = row[2]
-
-	//returnable := json.Marshal(user)
-	//returnable := json.Marshal(user)
 
 	return user, nil
 }
@@ -135,7 +91,7 @@ func AddUser(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError
 		fmt.Println(payload)
 		return Stair{}, &HandlerError{e, "Could'nt parse JSON", http.StatusInternalServerError}
 	}
-	con, err := sql.Open("mymysql", "tcp:localhost:3306*M7011E/root/jaam")
+	con, err := sql.Open("mymysql", "tcp:130.240.170.56:3306*mydb/dbadmin/eventdb")
 	if err != nil {
 		fmt.Println("Kunde inte öppna DB")
 		return nil, &HandlerError{err, "Internal server error", http.StatusInternalServerError}
@@ -158,7 +114,7 @@ func AddUser(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError
 
 	}
 
-	_, err = con.Exec("insert into Users( UserID, idToken, Username) values(?,?,?,?)", payload.UserID, payload.IdToken, payload.Username)
+	_, err = con.Exec("insert into User( UserID, idToken, Username) values(?,?,?,?)", payload.UserID, payload.IdToken, payload.Username)
 
 	if err != nil {
 		fmt.Println("Kunde inte lägga till :/")
@@ -167,21 +123,4 @@ func AddUser(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError
 
 	return payload, nil
 	//row, err := con.Query("select * from users where uid =?", param)
-}
-
-/*
-	Remove user from DB
-*/
-
-func RemoveUser(w http.ResponseWriter, r *http.Request) (interface{}, *HandlerError) {
-	param := mux.Vars(r)["id"]
-	id, e := strconv.Atoi(param)
-	if e != nil {
-		return nil, &HandlerError{e, "Id should be an integer", http.StatusBadRequest}
-	}
-	fmt.Println(id)
-	// this is jsut to check to see if the book exists
-
-	returnable := string("removeUser")
-	return returnable, nil
 }
