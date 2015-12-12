a76a4f6249ef0486a3dfa79e27c32d4f95ea15ba
Ztarmaestro <erkkar2@gmail.com>
2015-12-12 13:29:09 +0100

    added border

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -94,6 +94,7 @@ function createEventlist(data){
 		var a = document.createElement("a");
 		a.id = "a"+i;
 		a.href = "/show_event";
+		a.classname="panel-body";
     	document.getElementById("flow").appendChild(a);
 
 	}
