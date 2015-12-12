eabbb8e52b32a420329be122bdb2c81ecf794836
Ztarmaestro <erkkar2@gmail.com>
2015-12-12 10:06:22 +0100

    prints all in each list

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -40,8 +40,38 @@ function filterAllEvent(data){
 
 	for( var i=0, l=data.length; i<l; i++ ) {
     	console.log( data[i] );
+
+   		Event_name = document.getElementById('Event_name2');
+		Address = document.getElementById('Address2');
+		Zipcode = document.getElementById('Zipcode2');
+		date = document.getElementById('Date2');
+		Info = document.getElementById('Description2');
+		Photo= document.getElementById('Photo2');
+		//Preview = document.getElementById('Preview');
+
+		if(document.getElementById("Event_name2") != null){
+	    	Event_name.innerHTML = Event_name.innerHTML + data.Event_name;
+		}
+		if(document.getElementById("Address2") != null){
+	    	Address.innerHTML = Address.innerHTML + data.Address;
+		}
+		if(document.getElementById("Zipcode2") != null){
+	    	Zipcode.innerHTML = Zipcode.innerHTML + data.Zipcode;
+		}
+		if(document.getElementById("Date2") != null){
+	    	date.innerHTML = date.innerHTML + data.Date;
+		}
+		if(document.getElementById("Description2") != null){
+	    	Info.innerHTML = Info.innerHTML + data.Info;
+		}
+		if(document.getElementById("Photo2") != null){
+	    	Photo.src = data.Photo;
+		}
+
+		//Preview.innerHTML = Preview.innerHTML + data.Preview;
 	}
 	
+	/*
 	Event_name = document.getElementById('Event_name2');
 	Address = document.getElementById('Address2');
 	Zipcode = document.getElementById('Zipcode2');
@@ -70,5 +100,7 @@ function filterAllEvent(data){
 	}
 
 	//Preview.innerHTML = Preview.innerHTML + data.Preview;
+
+	*/
 
 }
