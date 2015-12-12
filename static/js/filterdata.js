76185c0c069ad246f9fe94f0f966aea35e923626
Ztarmaestro <erkkar2@gmail.com>
2015-12-12 11:00:20 +0100

    auto create div

--- static/js/filterdata.js
+++ static/js/filterdata.js
@@ -46,23 +46,26 @@ function filterAllEvent(data){
 	Photo= document.getElementById('Photo2');
 	//Preview = document.getElementById('Preview');
 
-/*
 	for( var i=0, l=data.length; i<l; i++ ) {
-    	console.log( data[i] );
-	    document.write("
-	    	<nav class="Event-flow">
-			    <div class="col-xs-10 col-sm-7" id="flow">
-			        <a href="/show_event" id="" class="list-group-item">
 
-			        	<h1 id="Event_name2"> </h1>
-			    		<p id="Date2">Date: </p>
-			    		<img src="" id="Photo2">
-			      
-			    	</a>       
-			    </div>
-			</nav>
-");
-*/
+		var a = document.createElement("a");
+    	a.className = list-group-item;
+    	a.id = linkevent;
+		a.href = "/show_event";
+    	document.getElementById("flow").appendChild(a);
+
+    	var head = document.createElement("h1");
+    	head.id = Event_name2;
+    	document.getElementById("linkevent").appendChild(head);
+
+    	var para = document.createElement("p");
+    	para.id = Date2;
+    	document.getElementById("linkevent").appendChild(para);
+
+    	var pic = document.createElement("img");
+    	pic.id = Photo2;
+    	pic.src = "";
+    	document.getElementById("linkevent").appendChild(pic);
 
 		if(document.getElementById("Event_name2") != null){
 	    	Event_name.innerHTML = Event_name.innerHTML + data[i].Event_name;
@@ -86,36 +89,4 @@ function filterAllEvent(data){
 		//Preview.innerHTML = Preview.innerHTML + data.Preview;
 	}
 	
-	/*
-	Event_name = document.getElementById('Event_name2');
-	Address = document.getElementById('Address2');
-	Zipcode = document.getElementById('Zipcode2');
-	date = document.getElementById('Date2');
-	Info = document.getElementById('Description2');
-	Photo= document.getElementById('Photo2');
-	//Preview = document.getElementById('Preview');
-
-	if(document.getElementById("Event_name2") != null){
-    	Event_name.innerHTML = Event_name.innerHTML + data.Event_name;
-	}
-	if(document.getElementById("Address2") != null){
-    	Address.innerHTML = Address.innerHTML + data.Address;
-	}
-	if(document.getElementById("Zipcode2") != null){
-    	Zipcode.innerHTML = Zipcode.innerHTML + data.Zipcode;
-	}
-	if(document.getElementById("Date2") != null){
-    	date.innerHTML = date.innerHTML + data.Date;
-	}
-	if(document.getElementById("Description2") != null){
-    	Info.innerHTML = Info.innerHTML + data.Info;
-	}
-	if(document.getElementById("Photo2") != null){
-    	Photo.src = data.Photo;
-	}
-
-	//Preview.innerHTML = Preview.innerHTML + data.Preview;
-
-	*/
-
 }
