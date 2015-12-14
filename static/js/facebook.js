0da06e3c607b764811d646f79d4113009e6f29b0
Ztarmaestro <erkkar2@gmail.com>
2015-12-14 01:12:27 +0100

    cleaning up main

--- static/js/facebook.js
+++ static/js/facebook.js
@@ -21,11 +21,6 @@
   //
   // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
- 
 
   // Load the SDK asynchronously
   (function(d, s, id) {
@@ -36,14 +31,16 @@
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
         addUser(response);
     });
   }
