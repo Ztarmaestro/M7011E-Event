3e4bd1b0ffe24d12e7a57a31f335af27ea8710ad
Ztarmaestro <erkkar2@gmail.com>
2015-12-14 20:31:57 +0100

    cookie check

--- static/js/Cookie.js
+++ static/js/Cookie.js
@@ -20,8 +20,8 @@ function addEventCookie(eID) {
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
+    var name = cname + "+";
+    var ca = document.cookie.split('=');
     for(var i=0; i<ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1);
@@ -32,7 +32,7 @@ function getCookie(cname) {
 
 function getEventid_Cookie(cname){
     var name = cname + "-";
-    var ca = document.cookie.split('/');
+    var ca = document.cookie.split(';');
     for(var i=0; i<ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1);
