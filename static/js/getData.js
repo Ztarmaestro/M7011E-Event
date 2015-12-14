3b325faafeed4188cc5000259427c3126b9970f2
Ztarmaestro <erkkar2@gmail.com>
2015-12-14 21:41:37 +0100

    cookie fix

--- static/js/getData.js
+++ static/js/getData.js
@@ -290,6 +290,9 @@ function redirect(data){
   console.log("sets event id to cookie");
 
   addEventCookie(data);
+  
+  var eventid = getEventid_Cookie("event");
+  console.log(eventid);
  
   //window.location.replace("/show_event");
 
